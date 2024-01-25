99bd095ec651f565a44453f6d31c56355edab616
adamsaleh1112 <pl231541@ahschool.com>
2024-01-25 08:03:31 -0500

    Commit

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ if detected is not None: # makes sure that it detected at least one
 	circles = numpy.uint16(numpy.around(detected)) # converts circles to x, y, and radius parameters
 
 	for parameter in circles[0, :]: # iterating through all circles' detected parameters
-		x, y, r = parameter[0], parameter[1], parameter[2] # setting x, y, and r to the respective parameters
+		x, y, r = parameter[0], parameter[1], parameter[2] # setting x, y, and radius to the respective parameters
 
 		# DRAWING MAIN CIRCLE
-		cv2.circle(image, (x, y), r, (0, 255, 0), 7) # args: image, coordinates, radius, color, stroke
+		cv2.circle(image, (x, y), r, (150, 255, 0), 7) # args: image, coordinates, radius, color, stroke
 
 		# DRAWING MID POINT
 		cv2.circle(image, (x, y), 1, (0, 0, 255), 10) # drawing a circle with a radius of 1 (dot) at same coordinates
 
 		# WINDOW
-		cv2.imshow("Detected Circle", image) # creating window called Detected Circle that displays output image
+		cv2.imshow("circle", image) # creating window called circle that displays output image
 		cv2.waitKey(0)
